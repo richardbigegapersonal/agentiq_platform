909b5a7d4da5791621a816f2e83c798f9b0222a2
Richard Bigega <89564469+richardbigegapersonal@users.noreply.github.com>
2025-08-01 09:18:42 -0500

    Create signin.js

---
+++ agentiq_fullstack/frontend/pages/signin.js
@@ -0,0 +1,5 @@
+// pages/sign-in/[[...index]].js
+import { SignIn } from "@clerk/nextjs";
+export default function Page() {
+  return <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />;
+}
