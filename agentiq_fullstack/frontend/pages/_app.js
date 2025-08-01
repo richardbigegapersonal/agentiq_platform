1d41af7441443ca60780b79c08b2551784d34825
Richard Bigega <89564469+richardbigegapersonal@users.noreply.github.com>
2025-08-01 07:22:57 -0500

    Update _app.js

--- agentiq_fullstack/frontend/pages/_app.js
+++ agentiq_fullstack/frontend/pages/_app.js
@@ -3,7 +3,7 @@ import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider {...pageProps}>
       <Component {...pageProps} />
     </ClerkProvider>
   );
